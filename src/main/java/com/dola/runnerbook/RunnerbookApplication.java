e33b5da4357d09d7b2313aed7904de4d0cd19ff3
Krzysztof <krzysztof_dolanski@epam.com>
2022-06-29 21:39:58 +0200

    runbook roads listed page

---
+++ src/main/java/com/dola/runnerbook/RunnerbookApplication.java
@@ -0,0 +1,13 @@
+package com.dola.runnerbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunnerbookApplication.class, args);
+    }
+
+}

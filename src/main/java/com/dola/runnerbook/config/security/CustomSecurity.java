0787b5d7dda030b1057775445481e01551fedb78
Krzysztof <krzysztof_dolanski@epam.com>
2022-07-01 14:35:03 +0200

    registration form

--- src/main/java/com/dola/runnerbook/config/security/CustomSecurity.java
+++ src/main/java/com/dola/runnerbook/config/security/CustomSecurity.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -32,5 +34,10 @@ public class CustomSecurity {
                 "/scripts/**",
                 "/styles/**"
         );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
